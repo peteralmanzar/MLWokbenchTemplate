28daa8951ed9284fa1637b44627de57839f99101
Peter Almanzar <peter.almanzar@gmail.com>
2024-07-23 08:29:49 -0400

    save point

--- src/Libraries/mltoolkit/modeltemplates.py
+++ src/Libraries/mltoolkit/modeltemplates.py
@@ -1,6 +1,33 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Flatten, Conv1D, MaxPooling1D, Conv2D, MaxPooling2D
 
+def GetModelTemplateMLPRegression(numberOfFeatures: int):
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(numberOfFeatures,)),
+        Dense(64, activation='relu'),
+        Dense(1)
+    ])
+
+    return model
+
+def GetModelTemplateMLPClassification(numberOfFeatures: int):
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(numberOfFeatures,)),
+        Dense(64, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+
+    return model
+
+def GetModelTemplateMLPClassification(numberOfFeatures: int, num_classes: int):
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(numberOfFeatures,)),
+        Dense(64, activation='relu'),
+        Dense(num_classes, activation='softmax')
+    ])
+
+    return model 
+
 def GetModelTemplateLSTM(numberOfSteps: int, numberOfFeatures: int):
     model = Sequential([
         LSTM(64, return_sequences=True, input_shape=(numberOfSteps, numberOfFeatures)),
@@ -10,15 +37,6 @@ def GetModelTemplateLSTM(numberOfSteps: int, numberOfFeatures: int):
         LSTM(32, return_sequences=True),
         Dropout(0.2),
         Dense(25, activation='relu'),
-        Dense(1)
-    ])
-
-    return model
-
-def GetModelTemplateMLP(numberOfFeatures: int):
-    model = Sequential([
-        Dense(64, activation='relu', input_shape=(numberOfFeatures,)),
-        Dense(64, activation='relu'),
         Dense(1)
     ])
 
@@ -37,7 +55,7 @@ def GetModelTemplate1DCNN(numberOfSteps: int, numberOfFeatures: int):
 
     return model
 
-def GetModelTemplate2DCNN(image_height: int, image_width: int):
+def GetModelTemplate2DCNN(image_height: int, image_width: int, num_classes: int):
     model = Sequential([
         Conv2D(32, (3, 3), activation='relu', input_shape=(image_height, image_width, 3)),
         MaxPooling2D((2, 2)),
